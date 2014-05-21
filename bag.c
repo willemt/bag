e9eb3777644d240c4aab0764513e239aa2d534a6
Willem Thiart <himself@willemthiart.com>
2014-05-21 17:15:08 +0900

    Added license; better documentation in header file

--- bag.c
+++ bag.c
@@ -1,32 +1,3 @@
-/*
- 
-Copyright (c) 2011, Willem-Hendrik Thiart
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * The names of its contributors may not be used to endorse or promote
-      products derived from this software without specific prior written
-      permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL WILLEM-HENDRIK THIART BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,9 +5,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "bag.h"
 
-/**
- * Init a bag and return it. Malloc space for it.
- * @return initialised bag */
 bag_t *bag_new()
 {
     bag_t *b;
@@ -78,9 +46,6 @@ void bag_put(bag_t * b, void* item)
     b->array[b->count++] = item;
 }
 
-/**
- * Remove one random item.
- * @return one random item */
 void* bag_take(bag_t * b)
 {
     int idx;
@@ -100,8 +65,6 @@ int bag_count(bag_t * b)
     return b->count;
 }
 
-/**
- * Free memory held by bag */
 void bag_free(bag_t * b)
 {
     free(b->array);
--- bag.h
+++ bag.h
@@ -1,3 +1,5 @@
+#ifndef BAG_H
+#define BAG_H
 
 typedef struct {
     void **array;
@@ -5,12 +7,22 @@ typedef struct {
     int count;
 } bag_t;
 
+/**
+ * Init a bag and return it. Malloc space for it.
+ * @return initialised bag */
 bag_t *bag_new();
 
+/**
+ * Free memory held by bag */
 void bag_free(bag_t * b);
 
 void bag_put(bag_t * b, void* i);
 
+/**
+ * Remove one random item.
+ * @return one random item */
 void *bag_take(bag_t * b);
 
 int bag_count(bag_t * b);
+
+#endif /* BAG_H */
